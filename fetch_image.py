e6c1999d63162556498305f8c86ec8f1319f159a
devmanInstrtuctor <75973241+devmanInstructor@users.noreply.github.com>
2021-10-25 16:01:38 +0400

    Убран ненужный импорт

--- fetch_image.py
+++ fetch_image.py
@@ -1,12 +1,10 @@
 import requests
 import os
 from urllib.parse import urlparse
-import urllib3
-urllib3.disable_warnings()
 
 def fetch_image(image_url, image_dir, image_name, payload = ''):
     filename = f'{image_dir}/{image_name}{get_file_ext_from_url(image_url)}'
-    response = requests.get(image_url, verify=False, params=payload)
+    response = requests.get(image_url, params=payload)
     response.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(response.content)
